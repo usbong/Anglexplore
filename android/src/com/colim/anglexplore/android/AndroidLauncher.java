f68e4fdd3c0137be2a12fdce9afe478015be76c4
hadrianpaulo <hadrianpaulo@gmail.com>
2015-12-13 20:30:17 +0800

    Initial commit and generation of default files from LibGDX. Includes all platforms.  Target Android SDK: 20; Min Android SDK: 8

---
+++ android/src/com/colim/anglexplore/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.colim.anglexplore.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.colim.anglexplore.AnglexploreGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AnglexploreGame(), config);
+	}
+}
---
+++ desktop/src/com/colim/anglexplore/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.colim.anglexplore.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.colim.anglexplore.AnglexploreGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AnglexploreGame(), config);
+	}
+}
